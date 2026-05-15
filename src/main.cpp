4e16082ca0dbe7cf46f8e8ebc87e8fb98066c2a3
Dustin Carmelo <dustcarmelo@yahoo.com>
2026-05-15 17:28:44 -0400

    added simple parameters

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include "helpers.h"
 
 void randomize(std::vector<int>& matrix, int width);
+void randomizeWithSeed(std::vector<int>& matrix, int width, int seed);
+void randomizeParameter(std::vector<int>& matrix, int& width, int seed, int power, bool bHashEnabled);
 
 int main()
 {
@@ -41,6 +43,9 @@ int main()
 	randomize(matrix, width);
 
 	float zoom = 3.5f;
+
+	int powerOfTwo = 0, seed = 0;
+	bool bEnableHash = false;
 
 	while (!WindowShouldClose())
 	{
@@ -86,6 +91,41 @@ int main()
 		ImGui::SeparatorText("PARAMETERS");
 		ImGui::TextWrapped("In this section, select the parameters desired, then press the button to confirm and randomize the matrix.");
 
+		ImGui::InputInt("Powers of Two", &powerOfTwo, 0, 0);
+		ImGui::SameLine();
+		ImGui::TextDisabled("(?)");
+		if (ImGui::IsItemHovered())
+		{
+			ImGui::BeginTooltip();
+			ImGui::SetTooltip("The power of 2 to set as the size of the matrix. Use 0 for default size: 128x128.");
+			ImGui::EndTooltip();
+		}
+
+		ImGui::InputInt("Seed", &seed, 0, 0);
+		ImGui::SameLine();
+		ImGui::TextDisabled("(?)");
+		if (ImGui::IsItemHovered())
+		{
+			ImGui::BeginTooltip();
+			ImGui::SetTooltip("The seed value to base the RNG off of. Use 0 for true random number generation.");
+			ImGui::EndTooltip();
+		}
+
+		ImGui::Checkbox("Enable hashing algorithm seed", &bEnableHash);
+		ImGui::SameLine();
+		ImGui::TextDisabled("(?)");
+		if (ImGui::IsItemHovered())
+		{
+			ImGui::BeginTooltip();
+			ImGui::SetTooltip("Enable this to use szudzik algorithm as the seed. Inputs to algorithm are the coordinates of the matrix.");
+			ImGui::EndTooltip();
+		}
+
+		if (ImGui::Button("Re-generate"))
+		{
+			randomizeParameter(matrix, width, seed, powerOfTwo, bEnableHash);
+		}
+
 		ImGui::End();
 
 
@@ -112,4 +152,47 @@ void randomize(std::vector<int>& matrix, int width)
 		{
 			matrix[y * width + x] = random(0, 1);
 		}
-};
+}
+
+void randomizeWithSeed(std::vector<int>& matrix, int width, int seed)
+{
+	for (int x = 0; x < width; x++)
+		for (int y = 0; y < width; y++)
+		{
+			matrix[y * width + x] = randomWithSeed(seed, 0, 1);
+		}
+}
+
+void randomizeParameter(std::vector<int>& matrix, int& width, int seed, int power, bool bHashEnabled)
+{
+	if (power > 0)
+	{
+		width = pow(2, power);
+		matrix.resize(width * width, 0);
+
+	}
+
+	if (bHashEnabled)
+	{
+		for (int x = 0; x < width; x++)
+			for (int y = 0; y < width; y++)
+			{
+				int szudzikValue = x >= y ? (x * x) + x + y : (y * y) + x;
+				int rand = randomWithSeed(szudzikValue, 0, 1);
+				matrix[y * width + x] = rand;
+			}
+	}
+	else
+	{
+		if (seed > 0)
+		{
+			randomizeWithSeed(matrix, width, seed);
+		}
+		else
+		{
+			randomize(matrix, width);
+		}
+	}
+
+	
+}
